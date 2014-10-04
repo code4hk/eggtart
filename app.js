4d61b1392f14eb39e46d5daab18a9ecb4267b6d1
chakming <chakming28@gmail.com>
2014-10-04 18:49:59 +0800

    fix templatecache issue

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-angular.module('eggTart', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'rumors']);
+angular.module('eggtart', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'rumors']);
 
-angular.module('eggTart').config(function($stateProvider, $urlRouterProvider) {
+angular.module('eggtart').config(function($stateProvider, $urlRouterProvider) {
 
     /* Add New States Above */
     $urlRouterProvider.otherwise('/latest');
 
 });
 
-angular.module('eggTart').run(function($rootScope) {
+angular.module('eggtart').run(function($rootScope) {
 
     $rootScope.safeApply = function(fn) {
         var phase = $rootScope.$$phase;
